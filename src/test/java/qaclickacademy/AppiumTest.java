cf1e76e7de4407e76a34c6e956b5a0868ece0c59
himanshu <himanshumishra.ds@gmail.com>
2020-09-22 10:23:22 +0530

    Asian guy in Development

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -11,10 +11,21 @@ public class AppiumTest {
 		System.out.println("modified changes 1");
 		System.out.println("modified changes 2");
 		System.out.println("modified changes 22");
-		System.out.println("modified changes 3");
+		System.out.println('"modified changes 3");
 		System.out.println("modified changes 4");
 		System.out.println("asdjbclajbl");
 		System.out.println("asdjbclajbl");
+		
+		System.out.println("modified chdanges 4");
+		System.out.println("asdjbcldsajbl");
+		System.out.println("asdjbc lajbl");
+		System.out.println("asdjbclajbl");
+		System.out.println("asdjbclajbl");
+		
+		tem.out.println("modified chdanges 4");
+		System.out.println("asdjbcldsddajbl");
+		System.out.println("asdjbc laddjbl");
+		System.out.println("this is for develop branch code test");
 		
 		
 		
